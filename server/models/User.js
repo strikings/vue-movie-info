2b28b41a29e3957cf3708c32190de3b4ea05d96f
吴东 <qianduanwd@163.com>
2020-05-07 20:34:45 +0800

    修改.gitignore

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize')
+
+module.exports = (sequelize, DataTypes) => {
+  class Model extends Sequelize.Model { }
+
+  // 初始值
+  Model.init({
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      validatae: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [8, 40]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User'
+  })
+  return Model
+}
